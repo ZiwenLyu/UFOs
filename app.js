283b6b78bbc56a334e656623e1c89c5501d11f2b
unknown <lyuziwen2@gmail.com>
2022-03-09 16:15:55 -0500

    create a function to handle filter data with clicks

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var tbody = d3.select("tbody");
 
 function buildTable(data){
     tbody.html("");
-    
+
     data.forEach((dataRow =>{
         let row = tbody.append("tr");
         Object.values(dataRow).forEach((val) => {
@@ -14,3 +14,14 @@ function buildTable(data){
         });
     }));
 };
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    buildTable(filteredData);
+    d3.selectAll("#filter-btn").on("click", handleClick);
+    buildTable(tableData);
+}
